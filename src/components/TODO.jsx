1b8397809012a58e49c6d81d3f828721096b94b7
Rohit <therohitsingh82@gmail.com>
2023-08-25 11:28:52 +0530

    final commit

--- src/components/TODO.jsx
+++ src/components/TODO.jsx
@@ -5,19 +5,44 @@ import {
   InputGroup,
   InputRightElement,
   SimpleGrid,
-  Text,
-  VStack,
+  Text
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 const TODO = () => {
-  const handleClick = () => {};
+  const [data,setData] = useState(" ")
+  const [item,setItem] = useState([]);
+  const handleClick = () => {
+    if(data === " ")
+    {
+      return ;
+    }
+    else{
+      setItem([...item,data]);
+      setData(" ");
+    }
+  };
+  const deleteHandler = (idx)=>{
+    const newList= item.filter((val ,index)=> index!== idx);
+    setItem(newList);
+  }
+  const deleteAllHandler = () =>{
+    setItem([]);
+  }
 
   return (
     <div>
-      <Text fontSize={'4xl'}>TODO LIST</Text>
+      <Text fontSize={'4xl'} color={'cyan.500'}>
+        TODO LIST
+      </Text>
       <InputGroup size="md">
-        <Input pr="4.5rem" placeholder="Enter password" />
+        <Input
+          pr="4.5rem"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+          type='text'
+          placeholder={'Add TODO... '}
+        />
         <InputRightElement width="4.5rem">
           <Button h="1.75rem" size="sm" onClick={handleClick}>
             Add
@@ -25,23 +50,42 @@ const TODO = () => {
         </InputRightElement>
       </InputGroup>
       <SimpleGrid columns={[1, 2, 3]} spacing="40px">
-        <Box
-          bg="#3a95c3"
-          height="125px"
-          m={'2rem'}
-          display="flex"
-          alignItems="center"
-          justifyContent="space-between"
-          flexDirection={'column'}
-          p={'10px'}
-          overflow={'auto'}
-        >
-          <Text color={'black'}>TODO</Text>
-          <Button width="90%" height="2.2rem" _hover={{ bg: '#2a6e90' }}>
-            REMOVE
-          </Button>
-        </Box>
+        {item.map((ele, idx) => {
+          return (
+            <Box
+              bg="#3a95c3"
+              height="125px"
+              m={'2rem'}
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              flexDirection={'column'}
+              p={'10px'}
+              overflow={'auto'}
+              key={idx}
+            >
+              <Text>{ele}</Text>
+              <Button
+                width="90%"
+                height="2.2rem"
+                _hover={{ bg: '#2a6e90' }}
+                onClick={() => deleteHandler(idx)}
+              >
+                REMOVE
+              </Button>
+            </Box>
+          );
+        })}
       </SimpleGrid>
+      <Button
+        m={'1rem'}
+        width="50%"
+        height="2.2rem"
+        _hover={{ bg: '#2a6e90' }}
+        onClick={deleteAllHandler}
+      >
+        REMOVE ALL
+      </Button>
     </div>
   );
 };
